4c3750ca14c11f98911576a0b38c13870db63024
Avrelle <anto.delahaye@gmail.com>
2023-01-05 15:24:36 +0100

    Create new announcement form

--- login.php
+++ login.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/login.css" type="text/css">
+    <link rel="stylesheet" href="css/form.css" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
-    <title>POO Immo - register</title>
+    <title>POO Immo - Login</title>
 </head>
 <!-- FORMULAIRE D'INSCRIPTION ADMIN -->
 <main>
